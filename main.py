d91560d1056aa70b76745a4cb2a022619680fc1f
Yasaman <ys.jafari98@gmail.com>
2019-06-09 10:03:48 +0430

    Fix empty line bug

--- main.py
+++ main.py
@@ -23,14 +23,16 @@ def read_link_data(file_name):
     f = open(file_name, "r")
     contents = f.read()
     data = contents.split("\n")
-    local_phys_addr = data[0].split(" ")
+    local_phys_address = data[0].split(" ")
     interfaces_info = []
     for info in data[1:]:
+        if info == '':
+            break
         address = info.split(" ")
         interface = LnxBody(address[0], address[1], address[2], address[3])
         interfaces_info.append(interface)
-    nodeData = LnxInfo(local_phys_addr[0], local_phys_addr[1], interfaces_info)
-    return nodeData
+    node_data = LnxInfo(local_phys_address[0], local_phys_address[1], interfaces_info)
+    return node_data
 
 
 def main(file_name):
