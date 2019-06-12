31e2043fca608ffdcd794172a044aa523e36e14b
Yasaman <ys.jafari98@gmail.com>
2019-06-12 18:26:36 +0430

    Set threads' deamon

--- main.py
+++ main.py
@@ -13,16 +13,15 @@ def main(file_name):
     this_node.register_handlers(200, this_node.update_distance_table)
     this_node.register_handlers(0, this_node.print_message)
     t1 = threading.Thread(target=this_node.send_table)
+    t1.daemon = True
     t2 = threading.Thread(target=this_node.link.receive_data)
+    t2.daemon = True
     t3 = threading.Thread(target=this_node.check_for_out_of_date)
+    t3.daemon = True
     t1.start()
     t2.start()
     t3.start()
     this_node.read_commands()
-    print("QUIT")
-    t1.join()
-    t2.join()
-    t3.join()
 
 
 if __name__ == "__main__":
